50ca173d561ac77237da467906279d5c9952be31
chigbolu <e.chigbolu@se15.qmul.ac.uk>
2017-03-03 00:10:00 +0000

    first_file

---
+++ find_patterns.py
@@ -0,0 +1,12 @@
+import csv as csv
+import math
+import subprocess
+import numpy as np
+
+
+raw_data = csv.reader(open('./matches.csv','rb'))
+header = trainData.next();
+
+data = [value for value in raw_data]
+
+
